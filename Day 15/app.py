99737d26b5b3ae16a27ce0a3ad4afef6ff387568
Afaque <afaqueumer33@gmail.com>
2022-10-24 12:36:07 +0530

    Day 15 update

---
+++ Day 15/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.header('st.latex')
+
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
+
+st.latex(r'''
+         \cos^2 \theta - \sin^2 \theta =  \cos (2\theta) 
+         ''')
